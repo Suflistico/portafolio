d1c7f39398f24af19d1b8fa535623289ffa3f96d
suflistico <ppanguinao.p@gmail.com>
2024-08-22 15:00:37 -0400

    carga de proyecto Portafolio

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,32 @@
-import React from 'react';
+/*import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './ProjectCard.css'; // Importa el archivo CSS
+import './ProjectCard.css';
 
 function ProjectCard({ title, description, link, preview }) {
    return (
+        <div className="col-md-4 mb-4">
+            <div className="card">
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src={preview} title={title}></iframe>
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href={link} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Ver Proyecto</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectCard;*/
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './ProjectCard.css';
+
+function ProjectCard({ title, description, link, preview }) {
+    return (
         <div className="col-md-4 mb-4">
             <div className="card">
                 <div className="embed-responsive embed-responsive-16by9">
